b4aa311b3cd5c2c0cd3e7f8cb40fa5a4e4fb3540
ivncvrter <164603028+ivncvrter@users.noreply.github.com>
2025-10-18 18:39:41 -0500

    Initial Commit

---
+++ PlayoffChallenge/PlayoffChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayoffChallengeApp.swift
+//  PlayoffChallenge
+//
+//  Created by Ian Carter on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PlayoffChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
