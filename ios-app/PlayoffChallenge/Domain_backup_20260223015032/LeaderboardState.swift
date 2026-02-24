0e74c3519718b829024d09cf416880b4ead57ee0
ivncvrter <164603028+ivncvrter@users.noreply.github.com>
2026-02-24 15:55:27 -0600

    Remove remaining non-iOS artifacts from ios-app directory

--- ios-app/PlayoffChallenge/Domain_backup_20260223015032/LeaderboardState.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// Domain leaderboard state enum.
-/// Maps from string wire representation.
-enum LeaderboardState: String, Equatable {
-    case pending = "pending"
-    case computed = "computed"
-    case error = "error"
-}
